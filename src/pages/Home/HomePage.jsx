b3007e170e9aaf4d422b3794b939b2adb843dd63
SuarezRoig <santiagosuarez2604@gmail.com>
2025-12-16 16:39:49 +0100

    Generating homepage with backend

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,15 +1,22 @@
 import axios from 'axios'
 import './HomePage.css'
+
+import { useEffect, useState } from 'react'
 import {Header} from '../../components/Header'
 import checkMark from '../../assets/images/icons/checkmark.png'
-import {products} from '../../../starting-code/data/products.js'
 
 
 export function Homepage(){
+
+  const [products, setProducts] = useState([])
+
+  useEffect(()=>{
     axios('http://localhost:3000/api/products').
     then((response)=>{
-      console.log(response.data)
+      setProducts(response.data)
     })
+  },[])
+
 
     return (
         <>
