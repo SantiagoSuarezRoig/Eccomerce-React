d926e6dcdb3642f51bb2f221047e1fe4f7753d7e
SuarezRoig <santiagosuarez2604@gmail.com>
2025-12-16 12:39:02 +0100

    Replacing A/href with Link/to and Making header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Homepage} from './pages/HomePage'
 import {CheckoutPage} from './pages/CheckoutPage'
 import {OrdersPage} from './pages/OrdersPage'
 import {TrackingPage} from './pages/TrackingPage'
+
 import {Routes , Route} from 'react-router'
 import './App.css'
 
@@ -10,11 +11,11 @@ function App() {
   return (
     <>
     <Routes>
+
       <Route index element={<Homepage />} />
       <Route path='checkout' element={<CheckoutPage />} /> 
       <Route path='orders' element={<OrdersPage />} /> 
       <Route path='tracking' element={<TrackingPage />} /> 
-
 
     </Routes>
      
